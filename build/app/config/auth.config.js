bd4af6518cd98d6ad6ecdfdb497cec97d660a5db
godgodwinter <kakadz.pro@gmail.com>
2023-02-08 00:43:30 +0700

    remove build

--- build/app/config/auth.config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.secret = void 0;
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-exports.secret = process.env.JWT_SECRET || "babeng-secret-key";
